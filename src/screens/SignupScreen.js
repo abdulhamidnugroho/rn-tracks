67ccf229a3767cdceacecbab4c9338cf008baeba
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2020-10-16 22:48:13 +0700

    sign up set up

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,60 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler';
+import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
-    return <>
-        <Text>SignupScreen</Text>
-        <Button title="Signin" onPress={() => navigation.navigate('Signin')} />
-        <Button title="mainFlow" onPress={() => navigation.navigate('mainFlow')} />
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    </>
+    return (
+        <KeyboardAvoidingView>
+            <ScrollView contentContainerStyle={styles.contentContainerStyle}>
+            <View style={styles.container}>
+                <Spacer>
+                    <Text h3>Sign Up for Tracker App</Text>
+                </Spacer>
+                <Input 
+                    label="Email" 
+                    value={email} 
+                    onChangeText={setEmail} 
+                    autoCapitalize="none" 
+                    autoCorrect={false} 
+                />
+                <Spacer />
+                <Input 
+                    label="Password" 
+                    value={password} 
+                    onChangeText={setPassword} 
+                    autoCapitalize="none" 
+                    autoCorrect={false} 
+                    secureTextEntry 
+                />
+                <Spacer>
+                    <Button title="Sign Up" />
+                </Spacer>
+            </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    )
 }
 
-const styles = StyleSheet.create({})
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: null
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+ 
+    },
+    contentContainerStyle: {
+        paddingVertical: 150,
+      },
+})
 
 export default SignupScreen
